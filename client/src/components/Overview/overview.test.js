a7aec45455aa3432043e081ebde9e0bb7af86494
gbmyt <gbmytabc1@gmail.com>
2023-01-20 12:41:31 -0500

    Add some failing tests to overview.test.js

--- client/src/components/Overview/overview.test.js
+++ client/src/components/Overview/overview.test.js
@@ -7,11 +7,14 @@ import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-// import userEvent from '@testing-library/user-event' // Unnecessary with fireEvent?
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 
-import Overview from './Overview.jsx';
+// import Overview from './Overview.jsx';
+// import Images from './Images.jsx';
+// import StyleCarousel from './StyleCarousel.jsx';
+// import StyleSelector from './StyleSelector.jsx';
+// import AddToCart from './AddToCart.jsx';
 
 // ==================================
 //     ⬇ ⭐ TESTING SETUP ⭐ ⬇
@@ -41,3 +44,41 @@ test('use jsdom in this test file', () => {
   const element = document.createElement('div');
   expect(element).not.toBeNull();
 });
+
+// ==================================
+//      ⬇ ⭐ UNIT TESTS ⭐ ⬇
+// ==================================
+test('loads and displays <Overview />', async () => {
+  // ARRANGE
+	render(<Overview />)
+
+  // ACT
+	await waitFor(() => screen.getByRole('heading'))
+
+  // ASSERT
+  expect(screen.getByRole('heading')).toHaveTextContent('Product Overview Widget')
+})
+
+test('loads and displays <Images />', async () => {
+	render(<Images />)
+	await waitFor(() => screen.getByRole('heading'))
+  expect(screen.getByRole('heading')).toHaveTextContent('Images Component')
+})
+
+test('<StyleCarousel />', async () => {
+	render(<StyleCarousel />)
+	await waitFor(() => screen.getByRole('heading'))
+  expect(screen.getByRole('heading')).toHaveTextContent('StyleCarousel Component')
+})
+
+test('<StyleSelector />', async () => {
+  render(<StyleSelector />)
+	await waitFor(() => screen.getByRole('heading'))
+  expect(screen.getByRole('heading')).toHaveTextContent('StyleSelector Component')
+})
+
+test('<AddToCart />', async () => {
+  render(<AddToCart />)
+	await waitFor(() => screen.getByRole('heading'))
+  expect(screen.getByRole('heading')).toHaveTextContent('AddToCart Component')
+})
