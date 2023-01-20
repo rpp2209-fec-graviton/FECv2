970005251d2e55cc4ec238c2b0d2ed6cc1dd21e6
gbmyt <gbmytabc1@gmail.com>
2023-01-20 12:14:38 -0500

    Added jest jsdom lib to package.json for testing with jest, confirmed jest tests run in new env

--- client/src/components/Overview/overview.test.js
+++ client/src/components/Overview/overview.test.js
@@ -1,3 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+// import userEvent from '@testing-library/user-event' // Unnecessary with fireEvent?
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
+import Overview from './Overview.jsx';
+
 test('test runs even when doing nothing', () => {
 	expect(true).toBe(true)
 })
+
+test('use jsdom in this test file', () => {
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
+});
+
