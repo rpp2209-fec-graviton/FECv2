545c1c7c8669f38dd178d662e1e1b67441a3fd86
Ari Tavakoli <aritavakoli98@gmail.com>
2023-01-21 14:56:04 -0800

    Context api, test-consumption, providerfile setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Overview from './components/Overview/Overview.jsx';
 import Reviews from './components/Reviews/index.js';
+import ContextProvider from './components/Context/ContextProvider.jsx';
 
 function App() {
   return (
     <>
-      <Overview />
-      <Reviews />
+      <ContextProvider>
+        <Overview />
+        <Reviews />
+      </ContextProvider>
     </>
   );
 }
