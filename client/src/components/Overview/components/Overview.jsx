e57658bb2fbea24afdad527860293f08c55ce527
gbmyt <gbmytabc1@gmail.com>
2023-01-23 13:57:08 -0500

    Initialize state for overview component. Shared state to be refactored out to a global state manager (todo)

--- client/src/components/Overview/components/Overview.jsx
+++ client/src/components/Overview/components/Overview.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductInfo from './ProductInfo.jsx';
 import Images from './Images.jsx';
 import StyleSelector from './StyleSelector.jsx';
@@ -7,9 +7,28 @@ import AddtoBag from './AddtoBag.jsx';
 import styles from '../overview.module.css';
 
 function Overview() {
+	// Private State
+	const [quantity, setQuantity] = useState(0);
+	const [size, setSize] = useState("S");
+	const [starred, setStarred] = useState(false);
+	const [selected, setSelected] = useState(0);
+	const [zoom, setZoom] = useState(0);
+
+	// Shared State (TODO: Move to global state)
+	const [starRating, setStarRating] = useState(0);
+
 	return (
 		<div className={styles.greyBorder}>
 			<h1>Product Overview Widget</h1>
+			<div className={styles.greyBorder}>
+			<h3>Initial State</h3>
+				<p>Quantity: {quantity}</p>
+				<p>Size: {size}</p>
+				<p>⭐ Starred? {starred ? "True" : "False" }</p>
+				<p>Currently Selected: {selected}</p>
+				<hr />
+				<p>Average Rating: {starRating}</p>
+			</div>
 			<Images />
 			<ProductInfo />
 			<StyleSelector />
