06aa2664386c8f34e510262b1dace291d7f91ae0
bennyv8 <bvanhr@gmail.com>
2023-01-22 17:46:40 -0800

    Appended QuestionsView Component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Overview from './components/Overview/Overview.jsx';
+import QuestionsView from './components/Questions/QuestionsView.jsx';
 
 function App() {
   return (
     <div>
      Hi
      <Overview />
+     <QuestionsView/>
     </div>);
 }
 
