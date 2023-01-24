e6f2f5e4de70ef326d98bda04d1ed6b5761fd008
gbmyt <gbmytabc1@gmail.com>
2023-01-24 17:51:36 -0500

    Patched by adding a conditional to check if productID query param is favicon.co for now. Will look into a better/best practice solution at a later date.

--- server/index.js
+++ server/index.js
@@ -12,15 +12,17 @@ app.use(logger)
 
 //generic route for url with any product id, ex: localhost:3000/71699
 app.get('/:productId', (req, res) => {
-  fetch(`products/${req.params.productId}`, function(err, productData) {
-    if(err) {
-      console.log('fetching error:', err);
-    } else {
-      //TODO: store product info in shared state (?)
-      res.send(productData.data);
-    }
-  })
-  //add actions for other components here
+  if (req.params.productId !== 'favicon.ico') {
+    fetch(`products/${req.params.productId}`, function(err, productData) {
+      if(err) {
+        console.log('fetching error:', err);
+      } else {
+        //TODO: store product info in shared state (?)
+        res.send(productData.data);
+      }
+    })
+    //add actions for other components here
+  }
 });
 
 process.on("SIGINT", () => {
