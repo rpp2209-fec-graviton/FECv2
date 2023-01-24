fcfe974b46223f834d35b3c9b24182193b3517bd
gbmyt <gbmytabc1@gmail.com>
2023-01-24 10:28:16 -0500

    Added basic modal styles

--- client/src/components/Overview/components/Modal.jsx
+++ client/src/components/Overview/components/Modal.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from '../overview.module.css';
 
 function Modal () {
 	return (
-		<>
+		<div className={`${styles.modal} ${styles["modal-hidden"]} `}>
 			<h3>Modal (Image Overlay) Component</h3>
 			<button>X Close</button>
-		</>
+		</div>
 	)
 };
 
