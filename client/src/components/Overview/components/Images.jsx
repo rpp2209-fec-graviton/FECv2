8593365dd60296101d4a716cdbca9eda527646ef
gbmyt <gbmytabc1@gmail.com>
2023-01-24 10:49:49 -0500

    Moved toggleModal function to its own utility file so it can be unit-tested easily and reused

--- client/src/components/Overview/components/Images.jsx
+++ client/src/components/Overview/components/Images.jsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import ThumbnailCarousel from './ThumbnailCarousel.jsx';
 import Modal from './Modal.jsx';
+import toggleModal from '../overview-utils/modal.js';
 
 function Images () {
 	const [show, setShow] = useState(false);
 
-	useEffect(() => {
-		console.log('modal state changed to:', show);
-	}, [show]);
-
-	const toggleModal = () => {
-		console.log('modal before', show);
-		setShow(show => !show);
-	};
-
 	return (
 		<div>
 			<h3>Images Component</h3>
-			<Modal toggleModal={toggleModal} show={show} />
+			<Modal
+				toggleModal={()=> toggleModal(setShow)}
+				show={show}
+				setShow={setShow}
+			>
+				<h3>This is a modal child</h3>
+			</Modal>
 			<ThumbnailCarousel />
 		</div>
 	)
--- client/src/components/Overview/components/Modal.jsx
+++ client/src/components/Overview/components/Modal.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from '../overview.module.css';
 
-function Modal ({ show, toggleModal }) {
+function Modal ({ show, setShow, toggleModal, children }) {
 	if (!show) {
-		return (<button onClick={toggleModal}>Show Modal</button>);
+		return (<button onClick={() => toggleModal(setShow)}>Show Modal</button>);
 	} else {
 		return (
 			<div className={`${styles.modal} ${styles["modal-hidden"]} `}>
 				<h3>Modal (Image Overlay) Component</h3>
-				<button onClick={toggleModal}>X Close</button>
+				{children}
+				<button onClick={() => toggleModal(setShow)}>X Close</button>
 			</div>
 		)
 	}
