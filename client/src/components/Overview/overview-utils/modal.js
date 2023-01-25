3ae8b0494a0348bff643f20ec4a15e9a9d3de076
gbmyt <gbmytabc1@gmail.com>
2023-01-25 11:43:30 -0500

    Removed extraneous console.log from modal.js

--- client/src/components/Overview/overview-utils/modal.js
+++ client/src/components/Overview/overview-utils/modal.js
@@ -4,9 +4,7 @@ export default function toggleModal(show, setShow) {
 	setShow(show => !show);
 	var fullImage = document.querySelector('#image-lg');
 	var modal = document.querySelector('#modal');
-	console.log('Modal', modal);
 
 	modal.classList.toggle(styles['modal-hidden']);
 	fullImage.classList.toggle(styles['modal-hidden']);
-	modal.classList.toggle(styles['modal-lg']);
 };
