596a5fdde8bcd7c4ba2ee57f64d7fed59dc1497e
gbmyt <gbmytabc1@gmail.com>
2023-01-24 21:13:24 -0500

    Added 'crop' for full size image

--- client/src/components/Overview/components/Images.jsx
+++ client/src/components/Overview/components/Images.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import ThumbnailCarousel from './ThumbnailCarousel.jsx';
-import styles from '../overview.module.css';
+import imageStyles from '../overview.module.css';
 
 function Images ({ selected, styles }) {
+
 	if (styles[selected.id]) {
 		const selectedProductStyles = styles[selected.id];
 		const selectedUrl = selectedProductStyles[0].photos[0].url;
 
 		return (
 			<div>
-				<img className={styles.overview__image} src={selectedUrl} />
+				<img
+					className={`
+						${imageStyles.overview__image}
+						${imageStyles['overview-border']}
+					`}
+					src={selectedUrl}
+				/>
 				<ThumbnailCarousel
 					selected={selected}
 					styles={styles}
 				/>
 			</div>
 		)
+	} else {
+		return (<h2>No Images to Display</h2>)
 	}
 
 };
