2d374620edc7c92eb7f6a48ab2c1f5fbf7f4b807
gbmyt <gbmytabc1@gmail.com>
2023-01-24 20:48:02 -0500

    Overview display real/full-size image instead of placeholder

--- client/src/components/Overview/components/Images.jsx
+++ client/src/components/Overview/components/Images.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ThumbnailCarousel from './ThumbnailCarousel.jsx';
+import styles from '../overview.module.css';
 
 function Images ({ selected, styles }) {
+	if (styles[selected.id]) {
+		const selectedProductStyles = styles[selected.id];
+		const selectedUrl = selectedProductStyles[0].photos[0].url;
 
-	return (
-		<div>
-			<h3>Images Component</h3>
-			<ThumbnailCarousel
-				selected={selected}
-				styles={styles}
-			/>
-		</div>
-	)
+		return (
+			<div>
+				<img className={styles.overview__image} src={selectedUrl} />
+				<ThumbnailCarousel
+					selected={selected}
+					styles={styles}
+				/>
+			</div>
+		)
+	}
 
 };
 
