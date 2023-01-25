b5b887f06b3f56633bac71b86b97fe6ce36a7839
Benny <62785232+bennyv8@users.noreply.github.com>
2023-01-25 00:14:03 -0800

    Update AnswersView.jsx

--- client/src/components/Questions/Answers/AnswersView.jsx
+++ client/src/components/Questions/Answers/AnswersView.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Answer from "./Answer.jsx";
-import SampleData from "/home/benny/hackreactor/front-end-capstone/FECv2/ExampleData/index.js"
+import SampleData from "../../../../ExampleData/index.js";
 
 function AnswersView({ question_id }) {
   /*Implementation Tasks
