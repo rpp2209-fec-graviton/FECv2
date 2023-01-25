872b96d28858289bf03520cd82f1970ddb03914d
gbmyt <gbmytabc1@gmail.com>
2023-01-24 21:58:44 -0500

    Added link to review link next to star rating with handleClick stub. To-Do: Link to Reviews Widget

--- client/src/components/Overview/components/StarRating.jsx
+++ client/src/components/Overview/components/StarRating.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import styles from '../overview.module.css';
 
 function StarRating() {
+	const handleClick = (e) => {
+		e.preventDefault();
+		console.log('Link to Reviews Component TODO');
+	};
+
 	return (
 		<div className={styles.stars__outer}>
 			<div className={styles.stars__inner}></div>
+			<a
+				className={styles.stars__a}
+				href="#"
+				onClick={handleClick}
+			>Read all reviews</a>
 		</div>
 	)
 }
