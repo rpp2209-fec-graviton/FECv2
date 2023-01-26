0feb7c05603cf26639f2fa8ff66ade2c30788e9a
Ari Tavakoli <aritavakoli98@gmail.com>
2023-01-26 09:53:42 -0800

    commit

--- client/src/components/Reviews/Review-List/index.js
+++ client/src/components/Reviews/Review-List/index.js
@@ -1,12 +1,24 @@
 import ListContainer from "./components/Review-List-Container/ListContainer.jsx";
 import React from "react";
+import useFetchProduct from "../../Hooks/useFetchProduct.js";
+
+
 
 
 export default function ReviewList({ children }) {
 
+
+  const { response, loading, error } = useFetchProduct({
+    method: 'get',
+    url: '/products'
+  });
+
+
   return (
     <div>
       <h1>Review List</h1>
+      {response}
+      {console.log(response, 'sd')}
       <ListContainer>
         {children}
       </ListContainer>
