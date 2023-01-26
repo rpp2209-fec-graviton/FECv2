d257267c6b242c75c408531d69eca0391bb376ed
gbmyt <gbmytabc1@gmail.com>
2023-01-26 12:13:02 -0500

    Added js-dom test to 'Testing Environment' spec, converted test() blocks to describe/it blocks. Nested tests for nested components within parent 'Overview Widget' describe block.

--- client/src/components/Overview/overview.test.js
+++ client/src/components/Overview/overview.test.js
@@ -10,26 +10,37 @@ import '@testing-library/jest-dom';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
+// Components
 import Overview from './components/Overview.jsx';
-// import Images from './components/Images.jsx';
-// import ProductInfo from './components/ProductInfo.jsx';
-// import Modal from './components/Modal.jsx';
-// import ThumbnailCarousel from './components/ThumbnailCarousel.jsx';
-// import StyleSelector from './components/StyleSelector.jsx';
-// import AddToBag from './components/AddToBag.jsx';
+import Images from './components/Images.jsx';
+import ProductInfo from './components/ProductInfo.jsx';
+import Modal from './components/Modal.jsx';
+import ThumbnailCarousel from './components/ThumbnailCarousel.jsx';
+import StyleSelector from './components/StyleSelector.jsx';
+import AddToBag from './components/AddToBag.jsx';
 
 // ==================================
 //    ⬇ ⭐ TESTING THE TESTS ⭐ ⬇
 // ==================================
 describe('Testing Environment', () => {
-  test('use jsdom in this test file', () => {
-    const element = document.createElement('div');
-    expect(element).not.toBeNull();
+  it('uses jsdom', () => {
+    const overview = document.createElement('div');
+    expect(overview).not.toBeNull();
+  });
+
+  it('uses jest-dom', function() {
+    document.body.innerHTML = `
+      <span data-testid="not-empty"><span data-testid="empty"></span></span>
+      <div data-testid="visible">Visible Example</div>
+    `
+
+    expect(screen.queryByTestId('not-empty')).not.toBeEmptyDOMElement()
+    expect(screen.getByText('Visible Example')).toBeVisible()
   });
 });
 
-// Make sure environment vars are loading
-describe('Environment Variables', () => {
+// Check Access To Env Vars
+describe('ENV Variables', () => {
   test('Receives process.env variables', () => {
     expect(process.env.API_URL).toBe('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp');
   });
@@ -39,9 +50,9 @@ describe('Environment Variables', () => {
 //     ⬇ ⭐ TESTING SERVER ⭐ ⬇
 // ==================================
 // https://testing-library.com/docs/react-testing-library/example-intro
-describe('Server Test', () => {
+describe('Server', () => {
   const server = setupServer(
-    rest.get('http://localhost:3000', (req, res, ctx) => {
+    rest.get('/*', (req, res, ctx) => {
       return res(
         ctx.json({ message: 'Loaded' }),
         ctx.status(200)
@@ -57,7 +68,7 @@ describe('Server Test', () => {
     server.use(
       // override the initial "GET /" request handler
       // to return a 500 Server Error
-      rest.get('http://localhost:3000', (req, res, ctx) => {
+      rest.get('/*', (req, res, ctx) => {
         return res(ctx.status(500))
       }),
     )
@@ -67,54 +78,63 @@ describe('Server Test', () => {
 
 // ==================================
 //      ⬇ ⭐ UNIT TESTS ⭐ ⬇
+//      Arrange, Assert, Act
 // ==================================
-describe('Widget / Layout / Rendering', () => {
-  test('loads and displays <Overview />', async () => {
-    // Arrange
-    render(<Overview />)
-
-    // Act
-    await waitFor(() => screen.getByRole('heading'))
-
-    // Assert
-    expect(screen.getByRole('heading')).toHaveTextContent('Product Overview Widget')
+describe('Overview Widget', () => {
+  it('Overview should render to browser', async () => {
+    render(<Overview />);
+    const overview = await screen.findByRole('heading', { name: /overview/i });
+    expect(overview).toBeInTheDocument();
+    expect(overview).toHaveTextContent('Product Overview Widget');
   })
 
-  // test('loads and displays <Images />', async () => {
-  // 	render(<Images />)
-  // 	await waitFor(() => screen.getByRole('heading'))
-  //   expect(screen.getByRole('heading')).toHaveTextContent('Images Component')
+  // describe('Images Component', () => {
+  //   it('Images ', async () => {
+  //     render(<Images />);
+  //     const images = await screen.findByRole('img');
+  //     expect(images).toBeInTheDocument();
+  //   })
   // })
 
-  // test('<ProductInfo />', async () => {
-  // 	render(<ProductInfo />)
-  // 	await waitFor(() => screen.getByRole('heading'))
-  //   expect(screen.getByRole('heading')).toHaveTextContent('ProductInfo Component')
+  // describe('ProductInfo Component', () => {
+  //   it('ProductInfo should render', async () => {
+  //     render(<ProductInfo />)
+  //     await waitFor(() => screen.getByRole('heading'))
+  //     expect(screen.getByRole('heading')).toHaveTextContent('ProductInfo Component')
+  //   })
   // })
 
-  // test('<Modal />', async () => {
-  // 	render(<Modal />)
-  // 	await waitFor(() => screen.getByRole('heading'))
-  //   expect(screen.getByRole('heading')).toHaveTextContent('Modal Component')
+  // describe('Modal Component', () => {
+  //   it('Modal should render', async () => {
+  //     render(<Modal />)
+  //     await waitFor(() => screen.getByRole('heading'))
+  //     expect(screen.getByRole('heading')).toHaveTextContent('Modal Component')
+  //   })
   // })
 
-  // test('<ThumbnailCarousel />', async () => {
-  // 	render(<ThumbnailCarousel />)
-  // 	await waitFor(() => screen.getByRole('heading'))
-  //   expect(screen.getByRole('heading')).toHaveTextContent('ThumbnailCarousel Component')
+  // describe('ThumbnailCarousel Component', () => {
+  //   it('ThumbnailCarousel should render', async () => {
+  //     render(<ThumbnailCarousel />)
+  //     await waitFor(() => screen.getByRole('heading'))
+  //     expect(screen.getByRole('heading')).toHaveTextContent('ThumbnailCarousel Component')
+  //   })
   // })
 
-  // test('<StyleSelector />', async () => {
-  //   render(<StyleSelector />)
-  // 	await waitFor(() => screen.getByRole('heading'))
-  //   expect(screen.getByRole('heading')).toHaveTextContent('StyleSelector Component')
+  // describe('StyleSelector Component', () => {
+  //   it('StyleSelector should render', async () => {
+  //     render(<StyleSelector />)
+  //     await waitFor(() => screen.getByRole('heading'))
+  //     expect(screen.getByRole('heading')).toHaveTextContent('StyleSelector Component')
+  //   })
+
   // })
 
-  // test('<AddToBag />', async () => {
-  //   render(<AddToBag />)
-  // 	await waitFor(() => screen.getByRole('heading'))
-  //   expect(screen.getByRole('heading')).toHaveTextContent('AddToBag Component')
+  // describe('AddToBag Component', () => {
+  //   it('AddToBag should render', async () => {
+  //     render(<AddToBag />)
+  //     await waitFor(() => screen.getByRole('heading'))
+  //     expect(screen.getByRole('heading')).toHaveTextContent('AddToBag Component')
+  //   })
   // })
 });
-
 
