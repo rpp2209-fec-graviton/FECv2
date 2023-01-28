4b566bf0e4efaa5b3f751cfb954f6616b9381027
Ari Tavakoli <aritavakoli98@gmail.com>
2023-01-28 11:05:21 -0800

    merge conflixts

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import Overview from './components/Overview/components/Overview.jsx';
 import Reviews from './components/Reviews/index.js';
-
 import ContextProvider from './components/Context/ContextProvider.jsx';
-import QuestionsView from './components/Questions/QuestionsView.jsx';
 import ReviewProvider from './components/Reviews/Context/ReviewProvider.jsx';
-
 import RelatedProducts from './components/RelatedProducts/RelatedProducts.jsx';
-
-import ContextProvider from './components/Context/ContextProvider.jsx';
-
 import QuestionsView from './components/Questions/index.js';
 
 
