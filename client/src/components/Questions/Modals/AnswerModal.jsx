0dd8a84fb12d7920b04eb9d01f7cb3f8a3634a8b
bennyv8 <bvanhr@gmail.com>
2023-02-01 02:39:59 -0800

    Initialized Modal
    Created a use Modal hook to control when shown
    Created Skeleton of Modal

--- client/src/components/Questions/Modals/AnswerModal.jsx
+++ client/src/components/Questions/Modals/AnswerModal.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { useQuestionsContext } from "../Context/QuestionsProvider.jsx";
 
-function AnswerModal(props) {
-  /*
-  Worry about later, not priority
-  */
+function AnswerModal({ isShowing, hide }) {
+
+  const [mount, setMount] = useState(false);
+  const { modalAnchor } = useQuestionsContext();
+
+
+  return (
+    isShowing ? ReactDOM.createPortal(
+      <React.Fragment>
+        <div className="modal-overlay" />
+        <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+          <div className="modal">
+            <div className="modal-header">
+              <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+              Submit A question
+            </div>
+            <p>
+              Hello, I'm a modal.
+            </p>
+          </div>
+        </div>
+      </React.Fragment>, modalAnchor.current
+    ) : null
+  )
 }
 
 export default AnswerModal;
