46f8cec4aa7047767d4d5e4f22b1c04a5c7a88fd
bennyv8 <bvanhr@gmail.com>
2023-02-01 17:21:10 -0800

    Initalized Questions Modal

--- client/src/components/Questions/QuestionsList/QuestionsList.jsx
+++ client/src/components/Questions/QuestionsList/QuestionsList.jsx
@@ -6,17 +6,20 @@ import AnswersList from ".././Answers/AnswersList.jsx";
 import useQuestionsList from "../hooks/useQuestionsList.jsx";
 import SearchBar from './SearchBar.jsx';
 import MoreAnsweredQ from "./MoreAnsweredQ.jsx";
+import QuestionModal from "../Modals/QuestionModal.jsx";
 
 //HOOKS
 import useCount from "../hooks/useCount.jsx";
 import usePage from "../hooks/usePage.jsx";
 import useMore from "../hooks/useMoreQA.jsx";
+import useModal from "../hooks/useModal.jsx";
 
 function QuestionsList(props) {
   const [page, makePage] = usePage(1);
   const [count, makeCount] = useCount(2);
   const [questionsList, getQList, filterQList] = useQuestionsList();
   const [more, showMore, toggleMore] = useMore();
+  const { isShowing, toggle } = useModal();
   /*Implementation tasks
     [] Expands 2 Questions at a time and Scrollable
     [] Confined to a single page, any longer should be scrollable
@@ -53,6 +56,11 @@ function QuestionsList(props) {
         })}
       </div>
       {questionsList.length > 0 && <MoreAnsweredQ {...{ count, more, showMore, makeCount, makePage, updateQList, checkQList }} />}
+      <button onClick={toggle}> Submit a Question + </button>
+      <QuestionModal
+        isShowing={isShowing}
+        hide={toggle}
+      />
     </div>
   )
 }
