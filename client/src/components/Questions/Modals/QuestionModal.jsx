d46d2d121323ee30a9f0c7dbef0cf0b51992395b
bennyv8 <bvanhr@gmail.com>
2023-02-01 22:27:25 -0800

    Should implement submit form button

--- client/src/components/Questions/Modals/QuestionModal.jsx
+++ client/src/components/Questions/Modals/QuestionModal.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 
 //HOOKS
 import { useQuestionsContext } from "../Context/QuestionsProvider.jsx";
+import { useProductContext } from "../../Context/ContextProvider.jsx";
 import useInput from "../hooks/useInput.jsx";
 
 function QuestionModal({ isShowing, hide, Question }) {
@@ -17,15 +19,39 @@ function QuestionModal({ isShowing, hide, Question }) {
   */
 
   const [mount, setMount] = useState(false);
+  const [productName, setProductName] = useState('');
   const { modalAnchor } = useQuestionsContext();
+  const { currentProductId } = useProductContext();
   const yourQuestion = useInput('');
   const nickname = useInput('');
   const yourEmail = useInput('');
 
   const submitForm = (event) => {
     event.preventDefault();
-    console.log('Implement submit', yourEmail.value, yourQuestion.value, nickname.value);
+    axios({
+      method: 'Post',
+      url: `http://localhost:3000/qa/questions`,
+      data: {
+        body: yourQuestion.value,
+        name: nickname.value,
+        email: yourEmail.value,
+        product_id: currentProductId
+      }
+    }).then((res) => {
+      console.log('Question', res.data);
+    })
   };
+
+  useEffect(() => {
+    axios({
+      method: 'POST',
+      url: `http://localhost:3000/products`,
+      data: { product_id: currentProductId }
+    })
+      .then((res) => {
+        setProductName(res.data.name)
+      })
+  }, [])
 
   return (
     isShowing ? ReactDOM.createPortal(
@@ -37,11 +63,20 @@ function QuestionModal({ isShowing, hide, Question }) {
             </button>
           </div>
           <form onSubmit={submitForm}>
-            <title>Ask Your Question</title>
-            <sub>About the [INSERT PRODUCT NAME HERE]</sub>
-            <textarea id="yourQuestion" placeholder="Question..." {...yourQuestion} rows="4" cols="50" maxLength={1000} required={true} />
-            <input placeholder="Example: jack543!" {...nickname} required={true} />
-            <input placeholder="Email" {...yourEmail} required={true} />
+            <h2>Ask Your Question</h2>
+            <h3>about the {productName}</h3>
+            <label>
+              Question:&nbsp;
+              <textarea placeholder="Type Your Question..." {...yourQuestion} rows="2" cols="25" maxLength={1000} required={true} />
+            </label>
+            <label>
+              Nickname:&nbsp;
+              <input placeholder="Example: jack543!" {...nickname} required={true} />
+            </label>
+            <label>
+              Email:&nbsp;
+               <input placeholder="Email" {...yourEmail} required={true} />
+            </label>
             <button type="button">Upload Photo</button>
             <button type="submit">Submit Answer</button>
           </form>
