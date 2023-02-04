f72ad50af5814a85aa682034fe58188434fbf47c
bennyv8 <bvanhr@gmail.com>
2023-02-04 01:28:55 -0800

    implemented helpful link for questions list

--- client/src/components/Questions/QuestionsList/Question.jsx
+++ client/src/components/Questions/QuestionsList/Question.jsx
@@ -3,17 +3,22 @@ import AnswerModal from "../Modals/AnswerModal.jsx";
 import useModal from "../hooks/useModal.jsx";
 import styles from "../questions.module.css";
 import useReport from "../hooks/useReport.jsx";
+import useHelpful from "../hooks/useHelpful.jsx";
 
 function Question({ q }) {
 
   const { isShowing, toggle } = useModal();
   const [isReported, reportQA, reportedStyle] = useReport(q.question_id, 'questions');
+  const [isHelpful, helpfulQA, helpfulStyle] = useHelpful(q.question_id, 'answers');
 
   return (
     <div className={styles.question}>
       <b data-testid="test-question">Q: {q.question_body}</b>
       <sub> Helpful?&nbsp;
-        <a>Yes({q.question_helpfulness})</a>&nbsp;
+        {isHelpful ?
+          <a style={helpfulStyle}>Yes({q.question_helpfulness + 1})</a> :
+          <a onClick={helpfulQA}>Yes({q.question_helpfulness})</a>
+        }
         | <a onClick={toggle}> Add Answer</a>&nbsp;
         | {isReported ?
           <a style={reportedStyle}>Reported</a> :
