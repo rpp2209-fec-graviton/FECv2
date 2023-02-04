ba90b06cda4b44e296bae0768f7162ddb3e59467
bennyv8 <bvanhr@gmail.com>
2023-02-04 02:26:23 -0800

    Stored helpfulness to local storage

--- client/src/components/Questions/hooks/useHelpful.jsx
+++ client/src/components/Questions/hooks/useHelpful.jsx
@@ -6,7 +6,13 @@ export default function useHelpful(QAid, helpfulCategory) {
   const category = helpfulCategory;
   const id = QAid;
 
-  const [isHelpful, setIsHelpful] = useState(false);
+  if (window.localStorage.getItem(category) === null) {
+    window.localStorage.setItem(category, JSON.stringify({}));
+  }
+
+  const localStorage = JSON.parse(window.localStorage.getItem(category));
+  console.log('before', localStorage);
+  const [isHelpful, setIsHelpful] = useState(localStorage[id]);
 
   var helpfulStyle = { fontWeight: 'bold', color: 'blue' };
 
@@ -17,6 +23,7 @@ export default function useHelpful(QAid, helpfulCategory) {
         url: `http://localhost:3000/qa/${category}/${id}/helpful`
       })
         .then((res) => {
+          window.localStorage.setItem(category, JSON.stringify({ ...localStorage, [id]: true }));
           setIsHelpful(true);
         })
         .catch((err) => {
