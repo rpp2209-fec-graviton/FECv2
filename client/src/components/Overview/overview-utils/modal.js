02fbfa21ee6ad392a384f799903f84c4a545fa95
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-02-05 16:24:59 -0500

    Added functionality to change modal cursor from pointer to crosshair on click to modal image

--- client/src/components/Overview/overview-utils/modal.js
+++ client/src/components/Overview/overview-utils/modal.js
@@ -2,21 +2,28 @@ import styles from '../overview.module.css';
 
 function toggleModal(show, setShow) {
 	setShow && setShow(show => !show);
+
+	// Get elements from DOM
 	var fullImage = document.querySelector('#image-lg');
 	var modal = document.querySelector('#modal');
 
+	// Toggle Image and Modal Visibility
 	modal.classList.toggle(styles['modal-hidden']);
 	fullImage.classList.toggle(styles['modal-hidden']);
 };
 
 function zoomModal() {
+	// Get elements from DOM
 	var container = document.getElementById('modal');
 	var modal = document.querySelector('#modal__content');
 
+	// Double Click to zoom image
 	container.addEventListener('dblclick', (e) => {
 		modal.style.transform = "scale(2.5)";
+		modal.style.cursor = "crosshair";
 	});
 
+	// Move mouse to pan around image
 	container.addEventListener('mousemove', (e) => {
 		const x = e.clientX - e.target.offsetLeft;
 		const y = e.clientY - e.target.offsetTop;
@@ -24,6 +31,7 @@ function zoomModal() {
 		modal.style.transformOrigin = `${x}px ${y}px`;
 	});
 
+	// Click one more time to revert to original image size
 	container.addEventListener('click', (e) => {
 		modal.style.transform = "scale(1)";
 	});
