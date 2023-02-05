4da9a1d7967a970405422a2cce5c3c796dbd187c
bennyv8 <bvanhr@gmail.com>
2023-02-04 18:24:02 -0800

    Set up mock server for testing

--- client/src/components/Questions/test-utils/mockHandlers.js
+++ client/src/components/Questions/test-utils/mockHandlers.js
@@ -1,6 +1,27 @@
 import { rest } from 'msw';
-import SampleData from "../../../../ExampleData/index.js";
+import SampleData from "../../../../../ExampleData/index.js";
 
 const hostPath = window.location.host;
 
-export const mswQuestionServer = setupSErver(...handlers);
+export const handlers = [
+  rest.post(`${window.location.origin}/products`, async (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(SampleData['products'])
+    )
+  }),
+
+  rest.post(`${window.location.protocol}//${window.location.hostname}/products`, async (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(SampleData['/products'])
+    )
+  }),
+
+  rest.get(`${window.location.origin}/qa/questions`, async (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(SampleData['/qa/questions'])
+    )
+  }),
+]
