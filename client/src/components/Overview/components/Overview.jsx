c47e6b95b21224124317c9ec879d54e48009650e
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-02-05 00:50:58 -0500

    Removed unnecessary context imports from Overview.jsx

--- client/src/components/Overview/components/Overview.jsx
+++ client/src/components/Overview/components/Overview.jsx
@@ -4,25 +4,16 @@ import axios from 'axios';
 import ProductInfo from './ProductInfo.jsx';
 import Images from './Images.jsx';
 import StyleSelector from './StyleSelector.jsx';
-import { useProductContext } from "../../Context/ContextProvider.jsx";
-import { useOverviewContext } from "../Context/OverviewProvider.jsx";
 
 import { fetch } from '../../../../../server/utils/fetch.js';
 import styles from '../overview.module.css';
 
 function Overview() {
-	// Global Context
-	const { currentProductId, ratingsAverage } = useProductContext();
-
-	// Overview Context
-	const { product, setProduct, allProductStyles, selectedStyle	} = useOverviewContext();
-
 	return (
-		currentProductId &&
 		<div className={`${styles.overview} ${styles.grid}`}>
 			<Images />
 			<StyleSelector />
-			{/* <ProductInfo /> */}
+			<ProductInfo />
 		</div>
 	);
 };
