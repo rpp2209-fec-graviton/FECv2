d63a3bb321433319b0377efa5fe0a7d9372a9e00
bennyv8 <bvanhr@gmail.com>
2023-02-14 03:17:59 -0800

    communicate with s3 database to get photo urls
    Will add photos in data storage of s3 in post requests
    Will retrieve photos from s3 data strage in get requests

--- server/routes/questions-route.js
+++ server/routes/questions-route.js
@@ -2,6 +2,60 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios');
 const { fetch } = require('.././utils/data-utils.js');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+  region: process.env.BUCKET_REGION
+});
+
+
+const addPhotosToS3 = async (req) => {
+  var imageName = `${req.file.originalname}${req.params.question_id}`
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: imageName,
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype
+  };
+  const command = new PutObjectCommand(params);
+
+  return s3.send(command)
+}
+
+const getPhotoUrl = async (photos=[]) => {
+  var photoUrls = [];
+
+  for (const photo of photos) {
+    //basic filtering for photos not inputted by me
+    if (photo.url.includes('https://')) {
+      photoUrls.push(photo);
+      continue;
+    }
+
+    const getObjectParams = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: photo.url,
+    }
+    const command = new GetObjectCommand(getObjectParams);
+    const url = await getSignedUrl(s3, command, { expiresIn: 300 });
+    if (url) {
+      photo.url = url;
+      photoUrls.push(photo);
+    }
+  }
+
+  return photoUrls;
+}
+
 
 var sortList = (array, key) => {
   return (
@@ -52,12 +106,32 @@ router.get('/questions/:question_id/answers', (req, res) => {
     if (err) {
       throw err;
     } else {
-      res.send(sortAList(sortList(payload.data.results, 'helpfulness')));
+      var newArray = payload.data.results.map(async (answer) => {
+        answer.photos = await getPhotoUrl(answer.photos);
+        return answer
+      })
+      Promise.all(newArray)
+      .then((result) => {
+        res.send(sortAList(sortList(result, 'helpfulness')));
+      })
+      .catch((err) => {
+        console.log(err);
+      })
     }
   })
 })
 
-router.post('/questions/:question_id/answers', (req, res) => {
+router.post('/questions/:question_id/answers', upload.single('photos'), async (req, res) => {
+  console.log('reqbody', req.body, 'reqfile', req.file, req.params.question_id);
+  if (!!req.file.originalname) {
+    var imageArray = [`${req.file.originalname}${req.params.question_id}s3`];
+    req.body.photos = imageArray;
+    await addPhotosToS3(req);
+  } else {
+    req.body.photos = [];
+  }
+
+
   try {
     axios({
       method: 'POST',
@@ -66,13 +140,15 @@ router.post('/questions/:question_id/answers', (req, res) => {
       data: req.body
     })
       .then((response) => {
-        console.log(response)
+        console.log(response.body);
         res.status(201).json(response.data);
       })
   } catch (error) {
     console.log(error);
     res.status(400).json('error');
   }
+
+  res.status(201);
 })
 
 router.put('/questions/:question_id/report', (req, res) => {
